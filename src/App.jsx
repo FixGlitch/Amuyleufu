c5f1770c8db12ed0925321cb84b2299fa1bb8d22
Lucas Blanco <126288618+FixGlitch@users.noreply.github.com>
2024-01-14 22:29:27 -0300

    si

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
-import {Route, Routes, useLocation} from "react-router-dom";
+import { Route, Routes, useLocation, Navigate } from 'react-router-dom';
 import './App.css';
-import NavBar from "./Components/NavBar/NavBar";
+import NavBar from './Components/NavBar/NavBar';
 import Footer from './Components/Footer/Footer';
 import Home from './Components/Home/Home';
 import Alojamientos from './Components/Alojamientos/Alojamientos';
@@ -9,19 +9,25 @@ import Vuelos from './Components/Vuelos/Vuelos';
 import Paquetes from './Components/Paquetes/Paquetes';
 
 function App() {
-  const {pathname} = useLocation();
-    return (
-      <div>
-        {pathname !== "/" && <NavBar/>}
-        <Routes>
-          <Route path='/home' element={<Home/>}/>
-          <Route path='/alojamientos' element={<Alojamientos/>}/>
-          <Route path='/vuelos' element={<Vuelos/>}/>
-          <Route path='/paquetes' element={<Paquetes/>}/>
-        </Routes>
-        {pathname !== "/" && <Footer/>}
-      </div>
-    )
-  }
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return (
+    <div>
+      {pathname !== '/' && <NavBar />}
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/alojamientos" element={<Alojamientos />} />
+        <Route path="/vuelos" element={<Vuelos />} />
+        <Route path="/paquetes" element={<Paquetes />} />
+      </Routes>
+      {pathname !== '/' && <Footer />}
+    </div>
+  );
+}
 
 export default App;
